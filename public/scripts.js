2c76578b43c6ecab340d4229baf130b577c2ba82
Jesuscbart <97801274+Jesuscbart@users.noreply.github.com>
2024-05-10 22:31:51 +0000

    new

---
+++ public/scripts.js
@@ -0,0 +1,81 @@
+const socket = io();
+const loginForm = document.getElementById("login-form");
+const registerForm = document.getElementById("register-form");
+const chatForm = document.getElementById("form");
+const input = document.getElementById("input");
+const messages = document.getElementById("messages");
+const loginButton = document.querySelector(
+  "button[onclick=\"showForm('login')\"]",
+);
+const registerButton = document.querySelector(
+  "button[onclick=\"showForm('register')\"]",
+);
+
+function showForm(type) {
+  if (type === "login") {
+    loginForm.style.display = "flex";
+    registerForm.style.display = "none";
+  } else if (type === "register") {
+    registerForm.style.display = "flex";
+    loginForm.style.display = "none";
+  }
+}
+
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  socket.emit("login", { username, password });
+});
+
+registerForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const username = document.getElementById("register-username").value;
+  const password = document.getElementById("register-password").value;
+  socket.emit("register", { username, password });
+});
+
+socket.on("login success", function () {
+  loginForm.style.display = "none";
+  registerForm.style.display = "none";
+  chatForm.style.display = "flex"; // Mostrar formulario de chat
+  loginButton.style.display = "none"; // Ocultar botón de login
+  registerButton.style.display = "none"; // Ocultar botón de registro
+});
+
+socket.on("login failed", function (message) {
+  alert("Login failed: " + message);
+});
+
+socket.on("register success", function (message) {
+  alert("Register successful: " + message);
+  showForm("login"); // After successful registration, show the login form
+});
+
+socket.on("register failed", function (message) {
+  alert("Register failed: " + message);
+});
+
+chatForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (input.value.trim() !== "") {
+    socket.emit("chat message", input.value);
+    input.value = "";
+  }
+});
+
+socket.on("chat message", function (msg) {
+  const item = document.createElement("li");
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+document.getElementById("close").addEventListener("click", function () {
+  socket.disconnect();
+  chatForm.style.display = "none"; // Ocultar formulario de chat
+  input.disabled = true; // Desactivar entrada de mensaje
+  loginButton.style.display = "block"; // Mostrar botón de login
+  registerButton.style.display = "block"; // Mostrar botón de registro
+  alert("You have been disconnected.");
+});
